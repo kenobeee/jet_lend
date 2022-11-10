3bbef1d9a229dc49c6c804f361b95f1054289a1e
KeNobeee <75133315+KeNobeee@users.noreply.github.com>
2022-11-11 01:44:31 +0600

    some refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import {App} from 'components/app';
 
-import './styles/index.scss';
+import './scss/index.scss';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
